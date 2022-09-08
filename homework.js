1dc98c641eea2da848409c71db7be095b7090151
Ayaan Diesow <ayaan-diesow@hotmail.com>
2022-09-08 18:31:14 +0100

    conditionals homework

---
+++ homework.js
@@ -0,0 +1,24 @@
+var name = "Barwaaqo"
+var member = "non-existent"
+var gender = "female"
+
+if (name === "Mohamed" && member === "brother" && gender === "male"){
+    console.log("This is my brother Mohamed")
+} else if (name === "Mohamed" && member === "grandad" && gender === "male") {
+    console.log("My grandad was also named Mohamed")
+} else if (name === "Safia" && member === "mom" && gender === "female"){
+    console.log("My mom is called Safia")
+} else if (name === "Abdulkadir" && member === "dad" && gender === "male") {
+    console.log("My dad is called Abdulkadir")
+} else if (name === "Ilyas" && member === "son" && gender === "male") {
+    console.log("This is my son Ilyas")
+} else if (name === "Anas" && member === "nephew" && gender === "male") {
+    console.log("My eldest nephew Anas")
+} else if (name === "Zahra" && member === "sister" && gender === "female") {
+    console.log("This is my little sister Zahra")
+} else if (name === "Yasmin" && member === "cousin" && gender === "female") {
+    console.log("This is my cousin Yasmin")
+} else if (name === "Zahra" && member === "grandma" && gender === "female") {
+    console.log("My only living grandparent is called Zahra")
+} else (name === "Barwaaqo" && member === "non-existent" && gender === "female") 
+    console.log("I don't have any family members called Barwaaqo")
---
+++ practice.js
@@ -0,0 +1,17 @@
+var gender = "boy"
+var sibling = "Mohamed"
+
+if (gender === "boy" && sibling === "Mohamed") {
+    console.log("This is my brother Mohamed")
+
+}else if ((gender != "girl" && sibling === "Zahra")) {
+    console.log("My little sister Zahra")
+
+}else if ((gender != "girl" && sibling === "Hamdi")) {
+    console.log("My sister Hamdi")
+
+}else {
+    console.log("I only have 1 brother")
+}
+
+
