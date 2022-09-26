30da6af7a840e6f4080033b230cd73e69765939d
Ayaan Diesow <ayaan-diesow@hotmail.com>
2022-09-26 12:30:56 +0100

    while loops

---
+++ while.js
@@ -0,0 +1,26 @@
+/*
+let num = 10
+
+while (num>=0){
+    console.log(num)
+    num--
+}
+
+let countDown = 50
+while (countDown>=-50){
+    console.log(countDown)
+    countDown--
+}
+
+let countUp = -100
+while (countUp<=1500){
+    console.log(countUp)
+    countUp++
+}
+*/
+
+let integer = 0
+while (integer<=500){
+    console.log(integer)
+    integer++
+}
